17798f5f081c99953868f8a8511875243e2553ae
Hakandede <hakanuludag27@gmail.com>
2022-03-15 18:43:30 +0300

    improved json formatting

--- server.js
+++ server.js
@@ -14,52 +14,52 @@ app.use(express.static('blob'));
 app.use(express.static('NotPong'));
 app.use(express.json({limit: "1mb" }));
 
-saveData.sort(function(a, b){
-  return b.score - a.score
-});
-function writeData(data)
-{
-	fs.writeFile('highscores.json', data, (err) => {
-	    if(err) throw err;
-	    console.log('file has been saved');
+
+ function correctArr()
+   {  
+     saveData.sort(function(a, b){
+       return b.score - a.score
+     });
+    for (i = saveData.length; i > 7; i--) {
+      saveData.splice(i, 1);
+      console.log(saveData[i]);
+      console.log(saveData.length);
+    }
+    const results = saveData.filter(element => {
+      if (Object.keys(element).length !== 0) {
+        return true;
+      }
+    
+      return false;
     });
-}
- 
+    saveData = results;
+    
+   }
+      
 
 app.get('/dicefight', function(request, response) 
 {
   response.sendFile(path.join(__dirname+'/NotPong/index.html'));
-
 });
 
+
 app.post('/CLItoSV', (request, response) => {
+    correctArr(); 
     console.log("I got response!");
+    console.log(request.body );
     saveData.push(request.body);
- var reqData = JSON.stringify(saveData);
-    writeData(reqData);           
+    var reqData = JSON.stringify(saveData);
+    fs.writeFile('highscores.json', reqData, (err) => {
+      if (err) throw (err);
+        console.log(reqData);
+        console.log("saved");
+    }
+    );  
 });
 
 app.get('/svtocli', (request, response) => {
-  saveData.sort(function(a, b){
-    return b.score - a.score
-});
-	for (i = 7; i < saveData.length;) {
-   	 saveData = saveData.slice(0, -1);
-	console.log("array uzunlugu"+saveData.length);
-}
-	for(var i = saveData.length; i > 0; i--)
-		{
-			console.log(saveData[i]+" bu "+i);
-			if(saveData[i] === {} )
-			{
-
-			 saveData.splice(i,1);
-			
-			}
-		}
-      	
+  correctArr(); 
   var data=JSON.stringify(saveData);
-	writeData(data);
-	response.send(data);
-	
-   });
+    response.send(data)
+   }
+);
