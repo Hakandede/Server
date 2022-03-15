7a32bb0aa3f180d13e1f1e5ae42f5b28b5cb5b0e
root <root@ip-172-31-30-162.eu-central-1.compute.internal>
2022-03-15 08:58:58 +0000

    commit

--- server.js
+++ server.js
@@ -17,30 +17,49 @@ app.use(express.json({limit: "1mb" }));
 saveData.sort(function(a, b){
   return b.score - a.score
 });
-for (i = 7; i < saveData.length;) {
-  saveData = saveData.slice(0, -1);
-  console.log(saveData);
-  console.log(saveData.length);
-} 
+function writeData(data)
+{
+	fs.writeFile('highscores.json', data, (err) => {
+	    if(err) throw err;
+	    console.log('file has been saved');
+    });
+}
+ 
 
 app.get('/dicefight', function(request, response) 
 {
   response.sendFile(path.join(__dirname+'/NotPong/index.html'));
+
 });
 
 app.post('/CLItoSV', (request, response) => {
     console.log("I got response!");
     saveData.push(request.body);
-    var reqData = JSON.stringify(saveData);
-    fs.writeFile('highscores.json', reqData, finishedd);
-    
+ var reqData = JSON.stringify(saveData);
+    writeData(reqData);           
 });
 
 app.get('/svtocli', (request, response) => {
   saveData.sort(function(a, b){
     return b.score - a.score
 });
+	for (i = 7; i < saveData.length;) {
+   	 saveData = saveData.slice(0, -1);
+	console.log("array uzunlugu"+saveData.length);
+}
+	for(var i = saveData.length; i > 0; i--)
+		{
+			console.log(saveData[i]+" bu "+i);
+			if(saveData[i] === {} )
+			{
+
+			 saveData.splice(i,1);
+			
+			}
+		}
+      	
   var data=JSON.stringify(saveData);
-    response.send(data)
-   }
-);
+	writeData(data);
+	response.send(data);
+	
+   });
